6492263b1ee17cabb1d770fefa863a443f6f7581
Mitul Shah <mitulshah@microsoft.com>
2021-11-03 00:47:02 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res
+        .status(200)
+        .send('Hello World')
+        .end();
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+        console.log('App listening...');
+});
